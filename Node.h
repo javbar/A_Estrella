0dce265a2d64bd01546ec950328e18f4fc7fe355
Deidara19 <deoxis_48@hotmail.com>
2015-02-26 22:28:00 -0600

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,27 +1,16 @@
 #ifndef NODE_H_INCLUDED
 #define NODE_H_INCLUDED
 
-#define ORDER 2
-#include<iostream>
-using namespace std;
-
 class Node
 {
 	public:
-		int value, height, EF;
-		Node *father;
-		Node *branch[ORDER];
+		char *origin;
+		char *destiny;
+		int cost;
+		Node *next;
 
 		Node();
-		~Node(){}
+		~Node();
 };
 
-Node::Node()
-{
-	father = NULL;
-	height = 1;
-	EF = 0;
-	for (int i = 0; i < ORDER; i++)
-		branch[i] = NULL;
-}
 #endif
